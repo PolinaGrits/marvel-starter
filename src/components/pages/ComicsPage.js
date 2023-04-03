091cb071e8e8c2ad24f7c5f0feeea120dc8f3a94
Polina <polinka256@gmail.com>
2023-04-03 15:34:17 +0300

    навигация по сайту + react router dom

---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,14 @@
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import AppBanner from "../appBanner/AppBanner";
+import ComicsList from "../comicsList/ComicsList";
+
+const ComicsPage = () => {
+    return (
+        <ErrorBoundary>
+            <AppBanner/>
+            <ComicsList/>
+        </ErrorBoundary>
+    )
+}
+
+export default ComicsPage;
